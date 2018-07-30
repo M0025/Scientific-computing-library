28f0dc241d6945625faf821fe99d722493978495
Alison <Alison_cmd@icloud.com>
2018-07-30 22:28:59 +0800

    Matplotlib
    Baseic Matplotlib;
    line bar hist fan scatter stackplot and so on ;
    draw a triangle function graphic.

---
+++ matplotlib/base.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import pi
+import csv
+# 用来正常显示中文标签
+plt.rcParams['font.sans-serif'] = ['SimHei']
+# 用来正常显示负号
+plt.rcParams['axes.unicode_minus'] = False
+print('base.py file has been excuted')
